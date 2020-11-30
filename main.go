daa324b8a8c3c3f80a9a8d66bfacc954969ff69c
ccerne <ccerne@vt.edu>
2020-11-29 19:23:02 -0500

    changed dest ip, port, uname, pwd, bind on 0.0.0.0

--- main.go
+++ main.go
@@ -19,19 +19,22 @@ import (
 const PrivKeyLocation string = "/Users/cernec1999/.ssh/id_rsa"
 
 // RemoteUsername is the username of the remote server
-// const RemoteUsername string = "dev"
-const RemoteUsername string = "ssmp"
+const RemoteUsername string = "dev"
+
+//const RemoteUsername string = "ssmp"
 
 // RemotePassword is the remote's password
-// const RemotePassword string = "j.#dM#N<`w>Ehv8:7\"4X8cpy\"f)2X5"
-const RemotePassword string = "&qWKKa$Lb*okfwtzhm8fGa2H&"
+const RemotePassword string = "j.#dM#N<`w>Ehv8:7\"4X8cpy\"f)2X5"
+
+//const RemotePassword string = "&qWKKa$Lb*okfwtzhm8fGa2H&"
 
 // RemoteAddr describes the remote server to connect to
-// const RemoteAddr string = "127.0.0.1:1234"
-const RemoteAddr string = "dadb0d.commentblock.com:22"
+const RemoteAddr string = "127.0.0.1:1234"
+
+//const RemoteAddr string = "dadb0d.commentblock.com:22"
 
 // ServerAddr is the address and port to bind to
-const ServerAddr string = ":22"
+const ServerAddr string = "0.0.0.0:22"
 
 // Dbg is if we are in debug mode
 const Dbg bool = true
